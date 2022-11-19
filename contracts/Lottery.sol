4712f0b58459f0847f263c3fe6d86b9dc9b71d8f
Omkar Sawant <omkar18@somaiya.edu>
2022-11-19 10:24:36 +0530

    Contract initiated

---
+++ contracts/Lottery.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity  ^0.8.17;
+
+contract Lottery{
+    address public manager;
+    address[] public players;
+
+    constructor(){
+        manager = msg.sender;
+    }
+
+    function enter() public payable{
+        require(msg.value > 0.1 ether);
+
+        players.push(msg.sender);
+    }
+
+    function random() private view returns (uint){
+        return uint(sha256(abi.encode(block.difficulty,block.timestamp,players)));
+    }
+
+    
+}
